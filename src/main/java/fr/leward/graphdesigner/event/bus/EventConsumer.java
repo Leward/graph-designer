37a54f8b1aca0d4218330eb9a735a8c8df27fbed
Paul-Julien Vauthier <pj82491@gmail.com>
2020-01-23 21:59:44 -0500

    Make it a functional interface

--- src/main/java/fr/leward/graphdesigner/event/bus/EventConsumer.java
+++ src/main/java/fr/leward/graphdesigner/event/bus/EventConsumer.java
@@ -1,10 +1,6 @@
 package fr.leward.graphdesigner.event.bus;
 
-/**
- * Created by Paul-Julien on 08/02/2015.
- */
+@FunctionalInterface
 public interface EventConsumer<T extends Event> {
-
-    public void consume(T event);
-
+    void consume(T event);
 }
